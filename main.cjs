fbfa845216db4173ecb1ed6d2d9173337b25b783
Pushkar Kanjani <pushkar2640@gmail.com>
2025-11-02 18:41:08 +0530

    configured laxmi chit fund

--- main.cjs
+++ main.cjs
@@ -1,10 +1,13 @@
-// main.js - This is the "brain" of your Electron app
+/*
+ * =====================================================================
+ * THE CORRECTED 'main.cjs' FILE WITH SECURITY POLICY
+ * =====================================================================
+ */
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron'); // <-- 1. Import 'session'
 const path = require('path');
 
-function createWindow () {
-  // Create the browser window.
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -14,14 +17,40 @@ function createWindow () {
     }
   });
 
-  // This loads your built React app (which we'll make in the last step)
   win.loadFile(path.join(__dirname, 'dist', 'index.html'));
 }
 
-app.whenReady().then(createWindow);
+// This 'whenReady' block is the new, important part
+app.whenReady().then(() => {
+  
+  // 2. This is the fix. We are modifying the security policy.
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        // This line tells Electron:
+        // "Allow this app to connect ('connect-src') to itself ('self')
+        // AND to our live backend server on Render."
+        'Content-Security-Policy': [
+          "connect-src 'self' https://the-bank-ers-backend.onrender.com"
+        ]
+      }
+    });
+  });
 
+  // 3. Create the window *after* setting the policy
+  createWindow();
+});
+
+// Standard Electron code (leave this as-is)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
 });
